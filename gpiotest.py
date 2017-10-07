6b4b95de5e99b20b31688668a6ae700cf52ad7ce
NMilind <nikhil.milind@hotmail.com>
2017-10-06 23:28:50 -0400

    GPIO Testing

---
+++ gpiotest.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python
+
+import time
+import RPi.GPIO as GPIO
+
+# Plug the ground into PIN 9
+# Plug the ain into PIN 3
+
+# Set mode to Board Pin Layout
+GPIO.setmode(GPIO.BOARD)
+
+# Set up PIN 3 as the main output, with an initial setting of 3.3V
+GPIO.setup(3, GPIO.OUT, initial = GPIO.HIGH)
+
+state = GPIO.LOW
+
+# Indefinite Blinking Behavior
+while (True):
+
+    GPIO.output(3, state)
+    state = GPIO.LOW if (state == GPIO.HIGH) else state = GPIO.HIGH
+    time.sleep(0.5)
