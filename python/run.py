5204e0f639fb5dce4993d3007732fff73f95e899
NMilind <nikhil.milind@hotmail.com>
2017-10-07 19:45:51 -0400

    Will it work? Probably not...

--- python/run.py
+++ python/run.py
@@ -4,6 +4,10 @@ import cv
 import numpy as np
   
 def doloop():
+
+    cascPath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv.CascadeClassifier(cascPath)
+
     global depth, rgb
     while True:
         # Get a fresh frame
@@ -12,9 +16,22 @@ def doloop():
         # Build a two panel color image
         d3 = np.dstack((depth,depth,depth)).astype(np.uint8)
         da = np.hstack((d3,rgb))
-        
+
+        image = cv.fromarray(np.array(da[::2,::2,::-1]))
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30,30)
+        )
+
+        for (x, y, w, h) in faces:
+            cv.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
+
         # Simple Downsample
-        cv.ShowImage('both', cv.fromarray(np.array(da[::2,::2,::-1])))
+        cv.ShowImage('both', image)
         cv.WaitKey(5)
-        
+
 doloop()
--- skeleton/skeltrack-test.c
+++ skeleton/skeltrack-test.c
@@ -15,6 +15,21 @@ typedef struct
 
 static void on_track_joints (GObject *obj, GAsyncResult *res, gpointer user_data) {
 
+    guint i;
+    BufferInfo *buffer_info;
+    guint16 *reduced;
+    gint width, height, reduced_width, reduced_height;
+    ClutterContent *content;
+    GError *error = NULL;
+
+    buffer_info = (BufferInfo *) user_data;
+    reduced = (guint16 *) buffer_info->reduced_buffer;
+    width = buffer_info->width;
+    height = buffer_info->height;
+    reduced_width = buffer_info->reduced_width;
+    reduced_height = buffer_info->reduced_height;
+
+    list = skeltrack_skeleton_track_joints_finish (skeleton, res, &error);
 }
 
 int main() {
