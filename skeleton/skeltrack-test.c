cc707171c96393b7fc251c68fead1879d7870016
NMilind <nikhil.milind@hotmail.com>
2017-10-07 16:45:25 -0400

    Skeltrack Stuff

--- skeleton/skeltrack-test.c
+++ skeleton/skeltrack-test.c
@@ -4,6 +4,19 @@
 
 static SkeltrackSkeleton *skeleton = NULL;
 
+typedef struct
+{
+    guint16 *reduced_buffer;
+    gint width;
+    gint height;
+    gint reduced_width;
+    gint reduced_height;
+} BufferInfo;
+
+static void on_track_joints (GObject *obj, GAsyncResult *res, gpoing user_data) {
+
+}
+
 int main() {
 
     return 0;
