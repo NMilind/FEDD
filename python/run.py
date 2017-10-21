0b65c105f32e974dc1ad232daff0bf7cf3d5cbc1
NMilind <nikhil.milind@hotmail.com>
2017-10-21 15:37:09 -0400

    Update

--- python/run.py
+++ python/run.py
@@ -1,39 +1,63 @@
 #!/usr/bin/env python
-from freenect import sync_get_depth as get_depth, sync_get_video as get_video
+import freenect as fn
 import cv
 import cv2
+import frame_convert2 as fc
 import numpy as np
-  
-def doloop():
+import time
 
-    cascPath = "haarcascade_frontalface_default.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
+def get_depth():
+	return fn.sync_get_depth()[0]
+	#return fc.pretty_depth_cv(fn.sync_get_depth()[0])
 
-    global depth, rgb
-    while True:
-        # Get a fresh frame
-        (depth,_), (rgb,_) = get_depth(), get_video()
-        
-        # Build a two panel color image
-        d3 = np.dstack((depth,depth,depth)).astype(np.uint8)
-        da = np.hstack((d3,rgb))
+def get_video():
+	return fn.sync_get_video()[0]
+	#return fc.video_cv(fn.sync_get_video()[0])
 
-        image = np.array(rgb[::2,::2,::-1])
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def frame_loop():
 
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(15, 15)
-        )
+	cascPath = "haarcascade_frontalface_default.xml"
+	faceCascade = cv2.CascadeClassifier(cascPath)
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
-            print "(%s, %s)" % (x, y)
+	#cv2.namedWindow("Depth")
+	cv2.namedWindow("Video")
 
-        # Simple Downsample
-        cv.ShowImage('both', cv.fromarray(image))
-        cv.WaitKey(5)
+	while True:
+		# Get a fresh frame
+		#depth = get_depth()
+		rgb = get_video()
 
-doloop()
+		# Convert to grayscale for face analysis
+		start = time.time()
+		gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+		end = time.time()
+		print("Gray Conversion: %s" % (end - start))
+
+		start = time.time()
+		faces = faceCascade.detectMultiScale(
+			gray,
+			scaleFactor=1.1,
+			minNeighbors=5,
+			minSize=(30,30)
+		)
+		end = time.time()
+		print("Face Detection: %s" % (end - start))
+
+		# Draw rectangles around faces
+		start = time.time()
+		for (x, y, w, h) in faces:
+			try:
+				cv2.rectangle(rgb, (x,y), (x+w, y+h), (0, 255, 0), 2)
+				print "(%s, %s)" % (x, y)
+			except:
+				pass
+		end = time.time()
+		print("Drawing Rectangles: %s" % (end - start))
+
+		# Simple Downsample
+		#cv2.imshow("Depth", depth);
+		cv2.imshow("Video", rgb);
+		if (cv2.waitKey(10) == 27):
+			break;
+
+frame_loop()
--- python/servotest.py
+++ python/servotest.py
@@ -34,10 +34,10 @@ def set_servo_pulse(channel, pulse):
 # Set frequency to 60hz, good for servos.
 pwm.set_pwm_freq(60)
 
-print('Moving servo on channel 0, press Ctrl-C to quit...')
+channel = int(input("Channel: "))
+
+print('Moving servo on channel %s, press Ctrl-C to quit...' % channel)
 while True:
     # Move servo on channel O between extremes.
-    pwm.set_pwm(0, 0, servo_min)
-    time.sleep(1)
-    pwm.set_pwm(0, 0, servo_max)
-    time.sleep(1)
+    servo_min = int(input("Set Pulse Length: "))
+    pwm.set_pwm(channel, 0, servo_min)
