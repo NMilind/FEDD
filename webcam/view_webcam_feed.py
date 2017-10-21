b199f9d90e9a26c21a1208daf2a22576c286522d
NMilind <nikhil.milind@hotmail.com>
2017-10-21 16:17:30 -0400

    Track Face in Webcam

--- webcam/view_webcam_feed.py
+++ webcam/view_webcam_feed.py
@@ -1,23 +1,37 @@
 import sys
 sys.path.append("/usr/local/lib/python3.5/site-packages")
 
-import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cascPath = sys.argv[1]
+faceCascade = cv2.CascadeClassifier(cascPath)
 
-while(True):
+video_capture = cv2.VideoCapture(0)
+
+while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, frame = video_capture.read()
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('Video', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
-cap.release()
+# When everything is done, release the capture
+video_capture.release()
 cv2.destroyAllWindows()
