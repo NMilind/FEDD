219de483f2e9ffd01578f3627d810fd22189b244
NMilind <nikhil.milind@hotmail.com>
2017-11-10 12:34:20 -0500

    Calculating dt in main loop

--- dist/servo_control.py
+++ dist/servo_control.py
@@ -8,7 +8,7 @@ import time
 get_time = lambda: int(round(time.time() * 1000))
 
 # Movement constant
-MOVE = 0.25
+MOVE = 1E-5
 
 #Initialize the PCA9685 Board using the default address (0x40)
 pwm = Adafruit_PCA9685.PCA9685()
