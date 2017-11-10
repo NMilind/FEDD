f094a95ac712a2ecf3d772a0585ba724a570aff6
NMilind <nikhil.milind@hotmail.com>
2017-11-10 11:59:44 -0500

    Update

---
+++ dist/servo_control.py
@@ -0,0 +1,65 @@
+# Import Adafruit library
+import Adafruit_PCA9685
+
+# Import threading modules
+import threading
+import time
+
+get_time = lambda: int(round(time.time() * 1000))
+
+# Movement constant
+MOVE = 1.5
+
+#Initialize the PCA9685 Board using the default address (0x40)
+pwm = Adafruit_PCA9685.PCA9685()
+
+# Configure min and max servo pulse lengths
+SERVO_MIN = 120
+SERVO_MAX = 660
+CHANNEL = 0
+
+# Current position
+current = 0
+currentThread = None
+
+# Make sure pulses stay in range
+def get_pulse(value):
+
+    if value < SERVO_MIN:
+        return SERVO_MIN
+    elif value > SERVO_MAX:
+        return SERVO_MAX
+    return value
+
+# Set initial value of the servo
+def initialize():
+    global current
+    current = (SERVO_MAX + SERVO_MIN) / 2.0
+    current = int(current)
+    pwm.set_pwm(CHANNEL, 0, current)
+
+# Handle input from the webcam
+def handle_input(v, dt):
+    global currentThread
+    if currentThread is None:
+        currentThread = threading.Thread(target=handle_input_async, args=(v, dt))
+        currentThread.start()
+    else:
+        currentThread.join()
+        currentThread = None
+        handle_input(v, dt)
+
+# Handle input asynchronously by lerping over the range in the given dt
+def handle_input_async(v, dt):
+    global current
+    start = get_time()
+    while get_time() <= start + dt:
+        dx = lerp(v, (get_time() / (start + dt)) * dt)
+        current += dx
+        current = get_pulse(current)
+        current = int(current)
+        pwm.set_pwm(CHANNEL, 0, current)
+
+# LERP helper function
+def lerp(v, t):
+    return MOVE * v * t
---
+++ dist/webcam.py
@@ -0,0 +1,91 @@
+import sys
+import time
+import servo_control
+
+# Set the path to the OpenCV installation on the RPi
+sys.path.append("/usr/local/lib/python3.5/site-packages")
+
+# Set up lambda function for current time in milliseconds
+get_time = lambda: int(round(time.time() * 1000))
+
+# Import OpenCV
+import cv2
+
+# Suggested resolution for webcam
+WIDTH = 213
+HEIGHT = 160
+
+# Cascade stylesheet containing face characteristics
+cascPath = "haarcascade_frontalface_default.xml"
+# Set the profile for OpenCV
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+# Begin video capture with suggested resolution
+video_capture = cv2.VideoCapture(0)
+video_capture.set(3, WIDTH)
+video_capture.set(4, HEIGHT)
+
+x_past = [0, 0, 0]
+v_past = [0, 0]
+
+start_time = 0
+end_time = 0
+
+# Initialize servo module
+
+servo_control.initialize()
+
+while True:
+
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+    # Obtain true width and height of each frame
+    height, width, channels = frame.shape
+
+    # Convert to gray scale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=0
+    )
+
+    # Draw a rectangle around the faces and compute deltas
+    for (x, y, w, h) in faces:
+
+        end_time = get_time()
+        delta_t = end_time - start_time
+        start_time = get_time()
+
+        # Remove last elements in history
+        x_past = x_past[0:2]
+        v_past = v_past[0:1]
+
+        # Calculate midpoints of face
+        MID_X = (x + (w / 2.0))
+        VEL_X = (MID_X - x_past[0]) / delta_t
+        #VEL_X = (VEL_X + v_past[0]) / 2.0
+
+        # Add new values to history
+        x_past = [MID_X] + x_past
+        v_past = [VEL_X] + v_past
+
+        # Send input to servo module to move servo
+        servo_control.handle_input(VEL_X, delta_t)
+
+        print("Face at X=%s | Movement: %s" % (MID_X, VEL_X))
+        cv2.rectangle(gray, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', gray)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
